8e0b8408e6c0273c70ba7355fd186e7cf8ce1b2d
chenxianfeng <192128698@qq.com>
2020-03-29 17:29:50 +0800

    cxf-cxf-cxf 5

--- src/main/java/com/atguigu/git/gitTest01.java
+++ src/main/java/com/atguigu/git/gitTest01.java
@@ -2,6 +2,6 @@ package com.atguigu.git;
 
 public class gitTest01 {
     public static void main(String[] args) {
-        System.out.println("陈先锋大帅哥！！！");
+        System.out.println("陈先锋AAAAAAAABBBBBBCCC大帅哥！！！");
     }
 }
