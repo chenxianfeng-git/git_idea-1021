0062f56e30cfdf0817624a857a725cd07d1c05b6
chenyang <1490023729@qq.com>
2020-03-29 17:17:31 +0800

    cy-cy-cxf 4

--- src/main/java/com/atguigu/git/test03.java
+++ src/main/java/com/atguigu/git/test03.java
@@ -2,7 +2,7 @@ package com.atguigu.git;
 
 public class test03 {
     public static void main(String[] args) {
-        System.out.println("猥琐的嗷大喵！！！");
-        System.out.println("猥琐的嗷大喵！！！");
+        System.out.println("AAAAAA！！！");
+        System.out.println("AAAAAA大喵！！！");
     }
 }
