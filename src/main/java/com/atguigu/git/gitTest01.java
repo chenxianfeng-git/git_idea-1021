568459de110aa9ff14ff5bb32b1fd83dc120a748
chenyang <1490023729@qq.com>
2020-03-29 16:43:35 +0800

    cy-cy-cxf 2

--- src/main/java/com/atguigu/git/gitTest01.java
+++ src/main/java/com/atguigu/git/gitTest01.java
@@ -3,5 +3,7 @@ package com.atguigu.git;
 public class gitTest01 {
     public static void main(String[] args) {
         System.out.println("陈先锋大帅哥！！！");
+        System.out.println("无敌！！！");
+        System.out.println("大帅哥！！！");
     }
 }
---
+++ src/main/java/com/atguigu/git/test01.java
@@ -0,0 +1,7 @@
+package com.atguigu.git;
+
+public class test01 {
+    public static void main(String[] args) {
+        System.out.println("无敌的小honey!!!");
+    }
+}
